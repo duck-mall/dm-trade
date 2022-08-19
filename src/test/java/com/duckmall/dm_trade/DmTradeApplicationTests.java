014d07f7348215fb5d716adb6b253781f75cb720
frank <1823427924@qq.com>
2022-08-19 17:52:08 +0800

    first

---
+++ src/test/java/com/duckmall/dm_trade/DmTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckmall.dm_trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
